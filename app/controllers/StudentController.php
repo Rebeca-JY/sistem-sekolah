f9a6e54185c3afdc61fd4c32983b277acb1982c0
Rebeca-JY <goyuu96@gmail.com>
2026-01-27 13:14:06 +0700

    wip: implement studnet controller

---
+++ app/controllers/StudentController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controller;
+
+class StudentController
+
+{ 
+    Public function index()
+    {
+        echo '<h1>Daftar Siswa</h1>';
+        echo '<p>Menampilkan daftar siswa</p>';
+    }
+    public function create ()
+    {
+        echo '<h1>Tambah Siswa</h1>';
+        echo '<p>Menampilkan form tambah  siswa</p>';
+    }
+
+}
+
+
+
+?>
