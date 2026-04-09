c5b312984e5a72382c038998fc8b8e80d69fe72f
Rebeca-JY <goyuu96@gmail.com>
2026-04-09 13:51:39 +0700

    feat: implement layout

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,23 +1,23 @@
 <?php
 namespace App\Core;
-
+ 
 class Controller
 {
-
-public function view(string $view, array $data = [])
-{
-    extract($data);
-
+ 
+    public function view(string $view, array $data = [])
+    {
+        extract($data);
+ 
         $view = str_replace(
-        '.',
-        '/',
-        $view
-    );
-
-        require_once "../app/views/{$view}.php";
+            '.',
+            '/',
+            $view
+        );
+        $content = "../app/views/{$view}.php";
+ 
+        require_once "../app/views/layouts/app.php";
     }
-
+   
 }
-
-
+ 
 ?>
--- app/views/students/edit.php
+++ app/views/students/edit.php
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Edit Siswa</title>
-    <link rel="stylesheet" href="/css/output.css">
-</head>
-
-<body class="min-h-screen flex flex-col bg-gray-100">
-<!-- Header start -->
-<header class="bg-violet-500 text-white">
-<div class="flex items-center justify-between container mx-auto p-4">
-            <a href="/students" class="font-bold text-xl">Sistem Sekolah</a>
-            <a href="/students/create" class="bg-white text-violet-500 px-4 py-2 rounded-lg">+ Tambah Siswa</a>
-        </div>
-</header>    
-<!-- Header End -->
-
-<!-- Main start -->
- <main class=" container mx-auto grow">
-    <div class="mt-8 space-y-2">
+ <div class="mt-8 space-y-2">
         <!-- card header start -->
             <div class="p-4 shadow rounded-lg bg-white">
                 <H1 class="text-2xl font-bold">Edit Siswa</H1>
@@ -57,16 +36,3 @@
     <!-- Card body end -->
         
     </div>
- </main>
-
-<!-- Footer start -->
-<footer class="bg-gray-800 text-white">
-    <div class="text-center p-4">
-        &copy <!?= date('Y')?> Sistem Sekolah - SMK KRISTEN IMMANUEL
-    </div>
-</footer>
-<!-- Footer End -->
-</body>
-
-
-</html>
